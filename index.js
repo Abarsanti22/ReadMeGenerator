4ed81bad55aad4bde98dffa35290a732080a7eba
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-09-26 09:47:34 -0400

    updated index.js file

--- index.js
+++ index.js
@@ -1,15 +1,144 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ 
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please provide a project title.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'What is the title of your repository?  (Required)',
+        validate: repoInput => {
+            if (repoInput) {
+                return true;
+            } else {
+                console.log('Please provide the title of your repository.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username?  (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please provide your Github username.');
+                return false;
+            }
+        }
+    },
+
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project? (Required)',
+        validate: descInput => {
+            if (descInput) {
+                return true;
+            } else {
+                console.log('Please provide a project description.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'documentation',
+        message: 'What are the operating instructions for your project? (Required)',
+        validate: docInput => {
+            if (docInput) {
+                return true;
+            } else {
+                console.log('Please provide instructions for using your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'screenshot',
+        message: 'Please provide relative path to screenshot image. (Required)',
+        validate: ssInput => {
+            if (ssInput) {
+                return true;
+            } else {
+                console.log('Please provide path to screenshot image.');
+                return false;
+            }
+        }
+    },
+    { 
+    type: "list",
+    message: "what license are you using? ",
+    name: "license",
+    choices: ["AAL", "ISC", "MIT", "NTP", "W3C"]
+    
+    },
+    {
+        type: 'input',
+        name: 'contact',
+        message: 'What is your contact information? (Required)',
+        validate: contactInput => {
+            if (contactInput) {
+                return true;
+            } else {
+                console.log('Please provide your contact information.');
+                return false;
+            }
+        }
+    },
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
+function init() {
+    inquirer.prompt(questions).then(function(userInput) {
+const markdownString = generateMarkdown(userInput)
+
+fs.writeFile('README.md', markdownString, function(err) {
+    if(err) {
+        console.log(err)
+    }
+    else {
+        console.log('Your ReadMe file has been created!')
+    }
+})
+    },
+    )}
+
 
 // TODO: Create a function to initialize app
-function init() {
-    questions()
-}
+// function init() {
 
+//     inquirer.prompt(questions).then(function(userInput) {
+        
+//         const markdownString = generateMarkdown(userInput)
+// }
+// )}
+
+// function init() {
+//     return inquirer.prompt(questions);
+// };
+
+// }
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,71 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title}
 
-`;
-}
+// `;
+// }
 
 module.exports = generateMarkdown;
+function generateMarkdown(data) {
+
+  //   return `# ${data.title}
+   return `# ${data.title}
+ 
+   ![badge](https://img.shields.io/github/languages/top/${data.github}/${data.repo})
+   <br> 
+   ![badge](https://img.shields.io/github/languages/count/${data.github}/${data.repo})
+   <br>
+   ![badge](https://img.shields.io/github/issues/${data.github}/${data.repo})
+   <br>
+   ![badge](https://img.shields.io/github/issues-closed/${data.github}/${data.repo})
+   <br>
+   ![badge](https://img.shields.io/github/last-commit/${data.github}/${data.repo})
+   <br>
+   ![badge](https://img.shields.io/badge/license-${data.license}-important)
+   
+   ## Description
+   
+    ${data.description}
+  
+   ## Documentation
+   ${data.documentation}
+  
+   ## Usage
+   ${data.usage}
+ 
+   ## Screenshot
+   ![Screenshot](${data.screenshot})
+ 
+   ## Features
+   ${data.features}
+   
+   # Acknowledgements
+   ${data.acknowledgements}
+     
+   ## License
+   ![badge](https://img.shields.io/badge/license-${data.license}-important)
+   <br>
+   Permission to use this application is granted under the ${data.license} license. <https://opensource.org/licenses/${data.license}>
+ 
+ 
+   ## Testing
+   ${data.testing}
+ 
+   ## Contact:
+ <a href="mailto:${data.email}">${data.email}</a>`
+  
+ }
+ 
+ module.exports = generateMarkdown;
